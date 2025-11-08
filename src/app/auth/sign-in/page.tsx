83cebc305df1f99cdec5fb6773138f3a9a1c2391
mdfarzam <mohammedfarzam02@gmail.com>
2025-11-09 00:12:30 +0530

    1:10:10

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { useAuth } from '@/hooks/user-auth'
 import Link from 'next/link'
 
 export default function LoginPage() {
--- src/hooks/user-auth.ts
+++ src/hooks/user-auth.ts
@@ -22,7 +22,7 @@ type SignInData = z.infer<typeof signInSchema>
 type SignUpData = z.infer<typeof signUpSchema>
 
 export const useAuth = () => {
-    const { signIn, singOut } = useAuthActions()
+    const { signIn, signOut } = useAuthActions()
     const router = useRouter()
     const [isLoading, setIsLoading] = useState(false)
 
@@ -46,20 +46,54 @@ export const useAuth = () => {
 
     const handleSignIn = async (data: SignInData) => {
         setIsLoading(true)
-
+        try {
+            await signIn("password", {
+                email: data.email,
+                password: data.password,
+                flow: 'signIn',
+            })
+            router.push('/dashboard')
+        } catch (error) {
+            console.error(error) 
+            signInForm.setError('password', {
+                message: 'Invalid email or password',
+            })
+        } finally {
+            setIsLoading(false)
+        }
         setIsLoading(false)
     }
+
+    
 
     const handleSignUp = async (data: SignUpData) => {
         setIsLoading(true)
-
-        setIsLoading(false)
+        try {
+            await signIn("password", {
+                email: data.email,
+                password: data.password,
+                name: `${data.firstName} ${data.lastName} `,
+                flow: 'signUp',
+            })
+            router.push('/dashboard')
+        } catch (error) {
+            console.error(error) 
+            signUpForm.setError('root', {
+                message: 'Failed to create account, email may already exist.',
+            })
+        } finally {
+            setIsLoading(false)
+        }
     }
 
-    const handleSignOut = async () => {
-        setIsLoading(true)
 
-        setIsLoading(false)
+    const handleSignOut = async () => {
+        try {
+            await signOut()
+            router.push('/auth/sign-in')
+        } catch (error) {
+            console.error('Sign out error:', error)
+        }
     }
 
 
